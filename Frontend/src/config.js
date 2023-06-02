381b528614007fc05e57dfa8400a5f5ee95c66e8
Rehan4778 <rehanashraf4778@gmail.com>
2023-06-02 15:37:14 +0500

    Charts updated

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "https://petify-shelter-server.vercel.app";
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = "https://petify-shelter-server.vercel.app";
+// const BASE_URL = 'http://localhost:5000'
 export default BASE_URL;
